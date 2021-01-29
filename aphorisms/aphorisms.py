a3589d8dc1528c3ea69f4ae56ad83c3633b89a4d
Alan Isaacs <xtrasenz@gmail.com>
2021-01-28 18:09:13 -0800

    Basic display of aphorisms

--- aphorisms/aphorisms.py
+++ aphorisms/aphorisms.py
@@ -1,6 +1,8 @@
 from flask import (Blueprint,
                    render_template)
-
+from markupsafe import Markup
+import json
+                   
 aphorisms_bp = Blueprint(
     'aphorisms_bp', __name__,
     static_folder='static',
@@ -11,4 +13,11 @@ aphorisms_bp = Blueprint(
 @aphorisms_bp.route('/aphorisms')
 def showAphorisms():
     """Display aphorisms page"""
-    return render_template('aphorisms.html')
+    # Get pointer to exiting file in read mode
+    # TODO: put file path in config so works everywhere
+    f = open("/home/whitman/atl/aphorisms/apho_list.json")
+    a_json = f.read()
+    f.close()
+    # Load json into a string variable
+    aphos = json.loads(a_json)
+    return render_template('aphorisms.html', aphos=aphos)
