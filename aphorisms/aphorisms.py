66f4fcfef6e323411d81a3133c5e3ae9c7ee3a40
Alan Isaacs <xtrasenz@gmail.com>
2021-01-29 14:45:00 -0800

    Make json file path system agnostic

--- aphorisms/aphorisms.py
+++ aphorisms/aphorisms.py
@@ -2,6 +2,7 @@ from flask import (Blueprint,
                    render_template)
 from markupsafe import Markup
 import json
+import os
                    
 aphorisms_bp = Blueprint(
     'aphorisms_bp', __name__,
@@ -14,8 +15,8 @@ aphorisms_bp = Blueprint(
 def showAphorisms():
     """Display aphorisms page"""
     # Get pointer to exiting file in read mode
-    # TODO: put file path in config so works everywhere
-    f = open("/home/whitman/atl/aphorisms/apho_list.json")
+    path_to_aphos = os.getcwd() + "/aphorisms/apho_list.json"
+    f = open(path_to_aphos)
     a_json = f.read()
     f.close()
     # Load json into a string variable
